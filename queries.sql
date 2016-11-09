dd81919e096ea683e88f030216febfab685ed908
qoops <akritianec@mail.ru>
2016-11-09 22:23:49 +0300

    template of a query for search

--- queries.sql
+++ queries.sql
@@ -1,2 +1,12 @@
 INSERT INTO hotel_chains VALUES(DEFAULT, чейннейм, овнерид)
 INSERT INTO locations VALUES(переменные..........)
+SELECT room_type.price, room_type.capacity, room_type.location
+FROM rooms 
+    JOIN room_types ON room_types.name = rooms.type_name
+                       room_types.location = rooms.location
+                       room_types.hotel_id = rooms.hotel_id
+    WHERE price < %s AND city = %s AND rooms.id NOT IN 
+    (SELECT id 
+     FROM accomodations
+     WHERE (check_in <= %checkin AND check_out >= %checkin) OR 
+           (check_out >= %checkout AND check_in <= %checkout))
