13c22c5f77c2d935be61179aed1e6ca2437a0039
qoops <akritianec@mail.ru>
2016-11-15 20:10:33 +0300

    added constraints

--- tables.sql
+++ tables.sql
@@ -1,17 +1,17 @@
  create table users(
     id serial PRIMARY KEY,
-    login varchar(50) UNIQUE,
+    login varchar(50) UNIQUE NOT NULL,
     password char(66) NOT NULL,
-    access_level integer NOT NULL,
+    access_level INTEGER NOT NULL,
     email varchar(124) UNIQUE NOT NULL
 );
 
 create table employees(
-    id INTEGER PRIMARY KEY references users(id),
-    first_name varchar(100),
-    last_name varchar(100),
-    salary INTEGER,
-    position varchar(100)
+    id INTEGER PRIMARY KEY references users(id) NOT NULL,
+    first_name varchar(100) NOT NULL,
+    last_name varchar(100) NOT NULL,
+    salary INTEGER NOT NULL,
+    position varchar(100) NOT NULL
 );
 
 create table visitors(
@@ -25,15 +25,15 @@ create table visitors(
 
 create table hotel_chains(
     chain_name varchar(100) PRIMARY KEY,
-    owner_id integer references users(id) ON DELETE CASCADE ON UPDATE CASCADE
+    owner_id integer NOT NULL references users(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 create table locations(
     chain_name varchar(100) references hotel_chains(chain_name) ON DELETE CASCADE ON UPDATE CASCADE,
-    city varchar(60),
-    location varchar(100),
+    city varchar(60) NOT NULL,
+    location varchar(100) NOT NULL,
     photo_path varchar(200),
-    admin_id integer references users(id),
+    admin_id integer references users(id) ON DELETE SET NULL ON UPDATE CASCADE,
     PRIMARY KEY(chain_name, location)
 );
 
@@ -42,19 +42,20 @@ CREATE DOMAIN room_status AS varchar(8)
 
 create table room_types(
     id SERIAL PRIMARY KEY,
-    chain_name varchar(100),
-    location varchar(100),
-    name varchar(100),
+    chain_name varchar(100) NOT NULL,
+    location varchar(100) NOT NULL,
+    name varchar(100) NOT NULL,
     price integer CHECK(price > 0) NOT NULL,
     capacity integer CHECK(capacity > 0) NOT NULL,
-    FOREIGN KEY (location, chain_name) REFERENCES locations(location, chain_name) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (location, chain_name) REFERENCES locations(location, chain_name) ON DELETE CASCADE ON UPDATE CASCADE,
+    UNIQUE (chain_name, location, name)
 );
 
 create table rooms(
     id serial PRIMARY KEY,
-    room_type INTEGER REFERENCES room_types(id) ON DELETE CASCADE,
-    roomNo integer,
-    status room_status
+    room_type INTEGER REFERENCES room_types(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    roomNo integer NOT NULL CHECK(roomNo > 0),
+    status room_status NOT NULL
 );
 
 create table reservations(
@@ -62,11 +63,28 @@ create table reservations(
     total integer CHECK(total > 0),
     check_in date NOT NULL,
     check_out date NOT NULL,
-    room_id integer,
-    visitor_id integer,
-    FOREIGN KEY (room_id) REFERENCES rooms(id),
-    FOREIGN KEY (visitor_id) REFERENCES visitors(id),
+    room_id integer NOT NULL REFERENCES rooms(id),
+    visitor_id integer NOT NULL REFERENCES visitors(id),
 	CONSTRAINT positive_duration CHECK (check_out > check_in)
+);
+
+create table cleaners(
+    staff_id serial PRIMARY KEY references employees(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    chain_name varchar(100) NOT NULL,
+    location varchar(100) NOT NULL,
+    first_name varchar(100) NOT NULL,
+    last_name varchar(100) NOT NULL,
+    salary integer CHECK(salary > 0) NOT NULL,
+    FOREIGN KEY (chain_name, location) REFERENCES locations(chain_name, location)
+);
+
+CREATE DOMAIN day_of_week AS char(3)
+    CHECK(VALUE IN ('MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'));
+
+create table cleaning(
+    id serial PRIMARY KEY REFERENCES rooms(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    cleaner integer references cleaners(staff_id),
+    weekday day_of_week NOT NULL
 );
 
 CREATE VIEW rooms_view AS
